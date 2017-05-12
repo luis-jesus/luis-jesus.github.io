026035ab8b5c0147e4e54766782c7e575394586d
Luis Jesus <jesus12jimmy13@gmail.com>
2017-05-12 02:58:58 -0500

    Agregando repositorio

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+// Application name	music
+// API key	4e1552c2d3f2fb89e8d065617174300d
+// Shared secret	8fe922a432aeeafa07987b6140110095
+// Registered to	Luis-Jesus
+
+
+export default {
+	appname:'music',
+	apiKey:'4e1552c2d3f2fb89e8d065617174300d',
+	secret:'8fe922a432aeeafa07987b6140110095',
+	registeredTo:'Luis-Jesus'
+}
